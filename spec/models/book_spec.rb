ab8070a000463537aafed02660bc85ae3b67843d
Gabriel Afflitto <gafflitto92@gmail.com>
2019-03-05 13:50:03 -0700

    Add Book Migration

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 describe Book, type: :model do
   describe "validations" do
     it {should validate_presence_of(:title)}
-    it {should validate_presence_of(:author)}
     it {should validate_presence_of(:description)}
   end
 end
